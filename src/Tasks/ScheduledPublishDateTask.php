70bcaa178b7dc6bcb8b19d78f209896205e50abe
Jason Loeve <jason@weareonfire.co.nz>
2022-08-09 16:28:43 +1200

    Adding object unpublish

--- src/Tasks/ScheduledPublishDateTask.php
+++ src/Tasks/ScheduledPublishDateTask.php
@@ -6,6 +6,15 @@ use SilverStripe\Dev\BuildTask;
 use SilverStripe\Versioned\ChangeSet;
 use SilverStripe\Versioned\ChangeSetItem;
 
+// You can set a published item back to draft in a number of ways. I don't know if the code you've shown would work or not.
+// My instinct is not since it's not actually altering the versioned DataObjects you're interested in - only the changesetitems
+// that refer to them.
+
+// You'll want to get $setItem->Object() and then perform an action on that - whether it's $object->doUnpublish() or
+// $object->deleteFromStage(Versioned::LIVE) or $object->doRollbackTo($setItem->VersionBefore) or one of the other many ways
+// to accomplish this, depending on whether you want before/after hooks to fire, new history to be added to indicate when
+// the 'rollback' occurred, etc.
+
 class ScheduledPublishDateTask extends BuildTask
 {
     public function run($request)
@@ -34,8 +43,20 @@ class ScheduledPublishDateTask extends BuildTask
 
                         foreach($setItems as $setItem) {
                             // $setItem->Object();
-                            // $setItem->doUnpublish();
-                            var_dump($setItem->Object());
+                            // $s->doUnpublish();
+
+
+
+
+
+                            if ($setItem->ObjectClass === $setItem->Object()->ClassName) {
+                                $setItem->Object()->doUnpublish();
+                            }
+                                //do sittress tuff
+
+
+
+                            //var_dump($setItem->Object());
                         }
                     }
                 }
