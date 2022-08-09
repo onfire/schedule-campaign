62d8e189673e74816ce901dd385522be4daf1321
Jason Loeve <jason@weareonfire.co.nz>
2022-08-09 16:02:46 +1200

    Refactoring to add end dates

--- src/Tasks/ScheduledPublishDateTask.php
+++ src/Tasks/ScheduledPublishDateTask.php
@@ -4,49 +4,44 @@ namespace Onfire\ScheduleCampaign\Tasks;
 
 use SilverStripe\Dev\BuildTask;
 use SilverStripe\Versioned\ChangeSet;
+use SilverStripe\Versioned\ChangeSetItem;
 
 class ScheduledPublishDateTask extends BuildTask
 {
     public function run($request)
     {
-        $countPublished = 0;
-        $countReverted = 0;
+        $count = 0;
 
         $now = date('Y-m-d');
 
         $sets = ChangeSet::get();
+        $items = ChangeSetItem::get();
 
         if ($sets) {
-//            foreach ($sets as $set) {
-//                if ($set->State === 'open' && $set->ScheduledPublishDate !== NULL) {
-//                    if ($now >= $set->ScheduledPublishDate) {
-//                        $set->publish();
-//                        $count++;
-//                    }
-//                }
-//            }
-
-            // Publish item, then close it
             foreach ($sets as $set) {
                 if ($set->State === 'open' && $set->StartPublishDate !== NULL) {
-                    if ($now >= $set->StartPublishDate) {
+                    if ($now >= $set->StartPublishDate && $now < $set->EndPublishDate) {
                         $set->publish();
-                        $countPublished++;
+                        $count++;
                     }
-                }
-            }
+                } elseif ($set->State === 'published' && $set->EndPublishDate !== NULL) {
+                    if ($now >= $set->EndPublishDate) {
 
-            // Revert item, then close it
-            foreach ($sets as $set) {
-                if ($set->State === 'published' && $set->EndPublishDate !== NULL) {
-                    if ($now <= $set->EndPublishDate) {
-                        $set->revert();
-                        $countReverted++;
+                        //$set->State = 'open';
+                        //$set->write();
+
+                        $setItems = $items->filter(['ChangeSetID' => $set->ID]);
+
+                        foreach($setItems as $setItem) {
+                            // $setItem->Object();
+                            // $setItem->doUnpublish();
+                            var_dump($setItem->Object());
+                        }
                     }
                 }
             }
         }
 
-        echo $countPublished . ' sets published | ' . $countReverted . ' sets reverted';
+        echo $count . ' items updated';
     }
 }
